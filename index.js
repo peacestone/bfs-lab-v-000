4a73d775df3618dbcb4700113a80f8d9f0f7b7d0
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-10-31 22:42:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-function bfs(rootNode, vertices, edges){
+function bfs(rootNode, vertices, edges) {
 
+  let queue = []
+
+  queue.push(rootNode)
+
+  while(queue.length !== 0){
+
+    console.log(queue)
+  let firstNode = queue.shift()
+  let adjacentNodes = findAdjacent(firstNode.name, vertices, edges)
+  console.log(adjacentNodes)
+  debugger
+  markDistanceAndPredecessor(rootNode, adjacentNodes)
+  queue.push(adjacentNodes)
+  }
 }
 
 function findAdjacent(node, vertices, edges) {
   let adjacentNodeName = []
   for (let i = 0; i < edges.length; i++) {
+    debugger
     if  (edges[i].includes(node)) {
       if (edges[i][0] === node) {
         adjacentNodeName.push(edges[i][1])
@@ -18,7 +33,7 @@ function findAdjacent(node, vertices, edges) {
 }
 
 function markDistanceAndPredecessor(predecessor, adjacentNodes) {
-  for (let i = 0; adjacentNodes.length > i; i++;) {
+  for (let i = 0; adjacentNodes.length > i; i++) {
     adjacentNodes[i].predecessor = predecessor
     adjacentNodes[i].distance = predecessor.distance + 1
   }
