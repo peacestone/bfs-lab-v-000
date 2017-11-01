83e381d4a742d3ae68dc1e36b01ef88297f0ce4c
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-10-31 21:12:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 function bfs(rootNode, vertices, edges){
 
 }
+
+function findAdjacent(node, vertices, edges) {
+  let adjacentNodeName = []
+  for (let i = 0; i < edges.length; i++) {
+    if  (edges[i].includes(node)) {
+      if (edges[i][0] === node) {
+        adjacentNodeName.push(edges[i][1])
+      }
+      else {
+        adjacentNodeName.push(edges[i][0])
+      }
+    }
+  }
+  console.log(adjacentNodeName)
+  return vertices.filter((vertice) => adjacentNodeName.includes(vertice.name) && vertice.distance === null)
+}
